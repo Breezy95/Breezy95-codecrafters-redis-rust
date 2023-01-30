02c327b71ff4b89ef1c4fc95dd391ac517b674c1
Breezy95 <fabrivlad1995@gmail.com>
2023-01-30 13:42:02 -0500

    test

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,25 @@
 // Uncomment this block to pass the first stage
- use std::net::TcpListener;
+ use std::net::{TcpListener,TcpStream};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-    //
+    
+    fn conn_handler(stream: TcpStream) {
+        let mut buf = [0;10]; 
+        let len = stream.peek().expect("peek op failed");
+        println!("value of the length of message is {}", len);
+    }
+
+
      let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     //
      for stream in listener.incoming() {
         match stream {
-             Ok(_stream) => {
+             Ok(succ_stream) => {
                  println!("accepted new connection");
+                 conn_handler(succ_stream);
              }
              Err(e) => {
                  println!("error: {}", e);
