174af6fe005352f4a9d5ae1d47057ab308830cd6
Breezy95 <fabrivlad1995@gmail.com>
2023-02-06 15:10:27 -0500

    test

--- src/main.rs
+++ src/main.rs
@@ -15,8 +15,8 @@ fn main() {
         let mut token: Vec<u8> = Vec::new();
         for i in 0 .. bytes_buff.len()-2{
             let term_slice = &bytes_buff[i..i+1];
-            //\r\n
-            if term_slice == b"\x72\x6E" {
+            //   \r\n
+            if term_slice == b"\x0D\x0A" {
                 break;
             }
             token.push(bytes_buff[i]);
