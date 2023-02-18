4d9c7f276f069bc07aae656551ac7f520c58faf7
Breezy95 <fabrivlad1995@gmail.com>
2023-02-18 03:35:55 -0500

    test

--- src/main.rs
+++ src/main.rs
@@ -175,8 +175,8 @@ fn conn_handler( stream: &mut TcpStream,kvpairs: Arc<Mutex<HashMap<String,String
                   }
 
         },
-        "get" => {let keyval = op_iter.next().unwrap().to_owned();
-            op_iter.next();
+        "get" => {let mut keyval = op_iter.next().unwrap().clone();
+            keyval = op_iter.next().unwrap().clone();
             println!("map size in get: {}", test_map.len());
             for key in test_map.keys(){
                 println!("keys set method: {key}");
