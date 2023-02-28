9aa1cb76afd26a340ae347281a56fbd2ee9d4401
Breezy95 <fabrivlad1995@gmail.com>
2023-02-27 20:43:15 -0500

    test

--- src/main.rs
+++ src/main.rs
@@ -132,7 +132,7 @@ fn tokenizer(bytes_buff: &mut Vec<u8>) -> Vec<String> {
             tokens.push(elem.unwrap().to_owned());
 
         }
-        let removelast = &tokens[..tokens.len()-2];
+        let removelast = &tokens[..tokens.len()-1];
         let token_vec =removelast.to_vec();
         println!("token vec contents {:?}", token_vec);
         return token_vec; 
