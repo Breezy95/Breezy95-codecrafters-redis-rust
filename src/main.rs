14a003f96248cf28c25e9b6ca627552a94db0a56
Breezy95 <fabrivlad1995@gmail.com>
2023-02-27 20:25:18 -0500

    test

--- src/main.rs
+++ src/main.rs
@@ -80,9 +80,8 @@ fn set_values(  kvmap:  Arc<Mutex<HashMap<String, RedisVal>>>, kv :&mut Peekable
 
         let val = iter.next();
         let mut insertedVal: RedisVal = RedisVal { value: val.unwrap().to_owned() , timer: None, endTime: None};  
-        
-        //let timer_flag = iter.next();
-        //let duration = iter.next();      
+        let subseq_vals: Vec<&String> =values.clone().collect();
+              
         
       
         kvp1.insert( key.to_owned(), insertedVal);
